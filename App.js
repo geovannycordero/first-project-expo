a6c77a1862ab481dfe2a0baa48880767d66ef98b
Geovanny Cordero <gcordero@pernixlabs.com>
2020-04-23 22:31:31 -0600

    App working with the contents of the tutorial

--- App.js
+++ App.js
@@ -1,10 +1,71 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
+import uploadToAnonymousFilesAsync from 'anonymous-files';
+// import { SplashScreen } from 'expo';
+
+// SplashScreen.preventAutoHide();
+// setTimeout(SplashScreen.hide, 5000);
+
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera roll is required!');
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+    if(Platform.OS === 'web') {
+      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri: null })
+    } else {
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri: null });
+    }
+  };
+
+  let openShareDialogAsync = async () => {
+    if(!(await Sharing.isAvailableAsync())) {
+      alert(`The image is available for sharing at: ${selectedImage.remoteUri}`);
+      return;
+    }
+    Sharing.shareAsync(selectedImage.remoteUri || selectedImage.localUri);
+  }
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Share this photo</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo}></Image>
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style={styles.button}>
+        <Text style={ styles.buttonText }>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,5 +76,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    marginTop: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
   },
 });
